f59f358e7cb19741c0e42393dc90460697d8f49e
ilanchezhian <ilanchezhiancse@gmail.com>
2018-12-17 08:24:39 +0530

    double _

--- classex/__/derived.py
+++ classex/__/derived.py
@@ -2,13 +2,15 @@ from classex.__.base import Base
 
 
 class Derived(Base):
+    a = "i am a"
+    _b = "i am b"
+    __c = "i am c"
     def print_values(self):
-        a = "i am a"
-        _b = "i am b"
-        __c = "i am c"
+      print()
 
 obj=Derived()
 print(" %s  %s  "%(obj.a,obj._b)) # and _b are overrided, but u cannot find
 print(obj._Base__c)
+print(obj._Derived__c)# this is the usage of double undercore
 print(dir(obj))
 print(obj.print_values())

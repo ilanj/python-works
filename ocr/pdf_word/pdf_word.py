e0e6a893a34b095b6b1856033194b69929d5ec28
ilanchezhianj <ilanchezhian18@gmail.com>
2022-11-27 11:41:31 +0530

    pdf to doc

---
+++ ocr/pdf_word/pdf_word.py
@@ -0,0 +1,9 @@
+from pdf2docx import Converter
+
+pdf_file = 'ila.pdf'
+docx_file = 'ila.docx'
+
+# convert pdf to docx
+cv = Converter(pdf_file)
+cv.convert(docx_file)      # all pages by default
+cv.close()

685a212b2a4ba9ca7404d7c7d2858c98c6365856
ilanj <ilanchezhian619@gmail.com>
2025-03-09 18:17:48 +0530

    code refractoring

--- programs/loopprograms/armstrong_no.py
+++ programs/loopprograms/armstrong_no.py
@@ -1,12 +1,13 @@
 #153 is an Armstrong number since 153 = 1*1*1 + 5*5*5 + 3*3*3.
 
 n=int(input("enter any no"))
+length = len(str(n))
 check_n=n
 armstrong=0
 while n != 0:
     rem=n % 10
     n = n // 10
-    armstrong=armstrong+(rem**3)
+    armstrong=armstrong+(rem ** length)
 if(check_n is armstrong):
     print("armstrong no")
 else:

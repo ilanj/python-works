20926815cf11daaeaac03e7e06d9bb47d1e314b1
ilanj <ilanchezhian619@gmail.com>
2025-03-10 11:29:43 +0530

    refractor codebase

--- programs/loopprograms/primeno.py
+++ programs/loopprograms/primeno.py
@@ -4,11 +4,9 @@ def check_prime_no(n):
         if(n%each==0):
             flag=1
             break
-<<<<<<< HEAD
+
     if flag == 0:
-=======
-    if flag is 0:
->>>>>>> 7aea316fb7211c19240808b49e999c9f2e0561f2
+
         print(n," is prime")
         return n
     else:
@@ -18,17 +16,14 @@ def generate_prime_no(n):
     for each in range(2,n+1):
         flag=0
         for itr in range(2,each):
-<<<<<<< HEAD
             if each % itr == 0:
                 flag=1
                 break
-        if flag == 0:
-=======
-            if each % itr is 0:
-                flag=1
-                break
+            if flag == 0:
+                if each % itr is 0:
+                    flag=1
+                    break
         if flag is 0:
->>>>>>> 7aea316fb7211c19240808b49e999c9f2e0561f2
             print(each)
 
 

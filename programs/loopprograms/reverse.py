d818fd3b3bad66994dc8f8ea77a3a40af7690c16
ilanj <ilanchezhian619@gmail.com>
2025-03-10 11:41:12 +0530

    refractoring

--- programs/loopprograms/reverse.py
+++ programs/loopprograms/reverse.py
@@ -1,18 +1,17 @@
+# sum
 n=eval(input("enter any no"))
 sum=0
 while n!=0:
     rem=(n%10)
     sum=sum+rem
     n=(n//10)#//-does a floor division where 0.9 becomes 0
-<<<<<<< HEAD
 print(sum)
 
+# reverse
 n=eval(input("enter any no"))
-sum=0
+rev=0
 while n!=0:
     rem=(n%10)
-    sum=sum*10 + rem
+    rev=rev*10 + rem
     n=(n//10)#//-does a floor division where 0.9 becomes 0
-=======
->>>>>>> 7aea316fb7211c19240808b49e999c9f2e0561f2
-print(sum)
+print(rev)

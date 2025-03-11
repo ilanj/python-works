921a75cd3349ceef37da465c8f5e50759d0b79fb
Ilanchezhian <ilanchezhian619@gmail.com>
2025-03-11 19:52:46 +0530

    Update sumofnos.py
    Update sumofnos.py

--- programs/loopprograms/sumofnos.py
+++ programs/loopprograms/sumofnos.py
@@ -1,4 +1,17 @@
+"""
+sum of first n nos
+"""
 sum = 0
 for i in range(6):
     sum = sum + i
+print(sum)
+
+"""
+sum of nos in list
+"""
+nos = [1,2,3,4,5]
+sum = 0
+for no in nos:
+    sum = sum + no
+
 print(sum)

00e9766d7bd8301df0b29c209e7f0038a7f9a9a9
Ilanchezhian <ilanchezhian619@gmail.com>
2025-03-12 20:07:48 +0530

    Update closestmatches.py

--- programs/stringconcepts/closestmatches.py
+++ programs/stringconcepts/closestmatches.py
@@ -1,12 +1,10 @@
 from difflib import get_close_matches
 
-
-def closeMatches(patterns, word):
+def closeMatches(word, patterns):
     print(get_close_matches(word, patterns))
-
 
 # Driver program
 if __name__ == "__main__":
     word = "ilanohe2hia"
     patterns = ["ilanchezhian", "sudharsan:", "mahendiran", "suresh kumae"]
-    closeMatches(patterns, word)
+    closeMatches(word, patterns)
